15448ade46553b0701d7ef55ee06f333391d529c
craig <craig.freiwald@gmail.com>
2022-07-19 21:07:30 -0400

    Modified for error checking in login and city api search to avoid 404 errors.

--- app.py
+++ app.py
@@ -2,19 +2,22 @@
 #                             IMPORTS
 # -----------------------------------------------------------------------------------------#
 import requests
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 # -----------------------------------------------------------------------------------------#
 #                       APP and DB CONFIG
 # -----------------------------------------------------------------------------------------#
 app = Flask(__name__)
-
+app.secret_key = '_5#y2L"F4Q8z'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weatherlocs.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 db = SQLAlchemy(app)
+
+# set url for api call to obtain city ID numbers
+url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=66e64fc4eb7e73b64c9e5eeccfcaed4c'
 
 
 # -----------------------------------------------------------------------------------------#
@@ -42,31 +45,53 @@ def home():
         return do_admin_login()
 
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['POST'])
 def do_admin_login():
     if request.form['username'] == 'admin' and request.form['password'] == 'password':
         session['logged_in'] = True
         return index()
     else:
+        flash('Login Unsuccessful. Please check you Username and Password and try Again')
         return home()
 
 
 @app.route('/weather', methods=['GET', 'POST'])
 def index():
+
     if request.method == 'POST':
         new_city = request.form.get('city')
-
+        cities = City.query.all()
         if new_city:
-            new_city_obj = City(name=new_city)
+            for city in cities:
+                if city.name == new_city:
+                    print('City name already exists within user list')
+                    flash('City name already exists within user list')
+                    return render_template('weather.html', usersList=createList())
+                elif searchAPI_DB(new_city):
+                    print('invalid city name entered')
+                    flash('Invalid city name entered, please check spelling and try again')
+                    return render_template('weather.html', usersList=createList())
+                else:
+                    new_city_obj = City(name=new_city)
+                    db.session.add(new_city_obj)
+                    db.session.commit()
 
-            db.session.add(new_city_obj)
-            db.session.commit()
+    return render_template('weather.html', usersList=createList())
 
+
+# bad city error check
+def searchAPI_DB(city):
+
+    try:
+        requests.get(url.format(city)).json()
+    except KeyError:
+        print('invalid city name entered')
+
+
+# method to create list of Cities using API call and from DB info
+def createList():
     # create list from DB model data
     cities = City.query.all()
-
-    # set url for api call to obtain city ID numbers
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=66e64fc4eb7e73b64c9e5eeccfcaed4c'
 
     # create list to pass objects to template api
     usersList = []
@@ -79,9 +104,7 @@ def index():
             'cityID': r['id']
         }
 
-        usersList.append(userLoc)
-
-    return render_template('weather.html', usersList=usersList)
+    return usersList
 
 
 if __name__ == '__main__':
